53af2c88d1674586d01737221b3f8995340e9f4e
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-07-24 21:16:36 +0530

    TD changed with LR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   stringFlowChart: any = '';
   constructor() {
     this.createFlowchart();
-  }  
+  }
 
   ngOnInit(): void {
     mermaid.initialize({});
@@ -21,14 +21,16 @@ export class AppComponent {
 
   createFlowchart() {
     this.flowChart = [
-       "graph LR",
-        "id1[Start] --> id2[Ques 1]",
-        "id2 --> id3[Ques 2] & id4[Ques 3]",
-        "id3 & id4 --> id5[Ques 4]",
-        "id5 --> id6",
-        "id6[Ques 5] --> id7[End]",
-        "id6 --> id2"
+      "%%{init: {'theme': 'base', 'themeVariables': {'primaryColor': 'rgba(229,96,115,.06)' , 'primaryTextColor' :  '#e56073', 'font-family': '-apple system, BlinkMacSystemFont, segoe ui, Oxygen, Ubuntu, Cantarell, fira sans, droid sans, helvetica neue, sans - serif'}}}%%",
+      'graph LR',
+      'id1(Start) --> id2(Ques 1)',
+      'id2 --> id3[Ques 2] & id4[Ques 3]',
+      'id3 & id4 --> id5[Ques 4]',
+      'id5 --> id6',
+      'id6[Ques 5] --> id7[End]',
+      'id6 --> id2',
     ];
-    this.stringFlowChart = this.flowChart.join("\n");
-  } 
+    this.stringFlowChart = this.flowChart.join('\n');
+    console.log(this.stringFlowChart);
+  }
 }
