4e0e462f4062a1d11ea0885ff863417406fb29c4
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-07-24 20:57:26 +0530

    Logic to build flowchart and template to fill the page with flowchart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import mermaid from 'mermaid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'graph-app';
+
+  flowChart: any;
+  stringFlowChart: any = '';
+  constructor() {
+    this.createFlowchart();
+  }  
+
+  ngOnInit(): void {
+    mermaid.initialize({});
+  }
+
+  createFlowchart() {
+    this.flowChart = [
+       "graph LR",
+        "id1[Start] --> id2[Ques 1]",
+        "id2 --> id3[Ques 2] & id4[Ques 3]",
+        "id3 & id4 --> id5[Ques 4]",
+        "id5 --> id6",
+        "id6[Ques 5] --> id7[End]",
+        "id6 --> id2"
+    ];
+    this.stringFlowChart = this.flowChart.join("\n");
+  } 
 }
